ceb2dee2d9f9cca01d6dad3b308e62482b787b54
tkhshmsy <tkhshmsy@gmail.com>
2022-05-27 13:59:32 +0900

    Fix leak at remove systemd service

--- src/plugins/multiprocesssystem/applicationmanager/systemd/systemdapplicationmanager.cpp
+++ src/plugins/multiprocesssystem/applicationmanager/systemd/systemdapplicationmanager.cpp
@@ -69,7 +69,7 @@ SystemdApplicationManager::SystemdApplicationManager(QObject *parent, Type type)
         if (status == QStringLiteral("destroyed")) {
             if (d->processMap.contains(application)) {
                 qDebug() << "remove process" << application.key();
-                d->processMap.remove(application);
+                delete d->processMap.take(application);
             }
         }
     });
