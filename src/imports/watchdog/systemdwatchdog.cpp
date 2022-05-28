df6df22a73cc3f596818c22714e6cb3d002514ce
tkhshmsy <tkhshmsy@gmail.com>
2022-05-28 13:35:34 +0900

    Enable SystemdWatchDog only when Systemd's WDT is running

--- src/imports/watchdog/systemdwatchdog.cpp
+++ src/imports/watchdog/systemdwatchdog.cpp
@@ -4,17 +4,36 @@
 #include <systemd/sd-daemon.h>
 #endif
 
+class SystemdWatchDog::Private
+{
+public:
+    bool enabled = false;
+};
+
 SystemdWatchDog::SystemdWatchDog(QObject *parent)
     : QObject(parent)
+    , d(new Private)
 {
 #if !QT_CONFIG(systemd_watchdog)
-    qWarning("SystemdWatchDog is disabled");
+    qWarning("SystemdWatchDog is not supported");
+#else
+    // WATCHDOG_USEC is set by systemd when WatchdogSec is set in its service-unit
+    if (qEnvironmentVariableIsSet("WATCHDOG_USEC")) {
+        qInfo("SystemdWatchDog is available");
+        d->enabled = true;
+    } else {
+        qInfo("SystemdWatchDog is not available");
+    }
 #endif
 }
+
+SystemdWatchDog::~SystemdWatchDog() = default;
 
 void SystemdWatchDog::pang()
 {
 #if QT_CONFIG(systemd_watchdog)
-    sd_notify(0, "WATCHDOG=1");
+    if (d->enabled) {
+        sd_notify(0, "WATCHDOG=1");
+    }
 #endif
 }
